98ad97b5ef46935bced0f578a41a2ea3d0d769c7
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-08-19 17:53:00 +0300

    Experiment with things

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,14 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
     <span>Build date: {{ buildDate }}</span>
+    <div>Brains cells died while thinking about this project: {{ count }}</div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { timer, Subscription } from 'rxjs';
 
 @Component({
   components: {
@@ -16,8 +18,23 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
   },
 })
 export default class Home extends Vue {
+  count: number = 0;
+  countSub?: Subscription;
+
   get buildDate() {
     return process.env.BUILD_TIME;
+  }
+
+  mounted() {
+    this.countSub = timer(100, 100).subscribe(_ => {
+      this.count++;
+    });
+  }
+
+  destroyed() {
+    if (this.countSub) {
+      this.countSub.unsubscribe();
+    }
   }
 }
 </script>
