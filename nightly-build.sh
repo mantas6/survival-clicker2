ca9bac4153c8bd0d09f5fcc425f48a58ae5f292f
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-08-19 12:59:15 +0300

    Fix nighly build script

--- nightly-build.sh
+++ nightly-build.sh
@@ -2,7 +2,13 @@
 
 # Automatically fetches updates from the repo and builds them
 
+# First argument is path to repository files
+# Second argument is public folder of the web server
+
+cd $1
 git checkout -- .
 git pull origin master
 npm i
 npm run build
+rm -rf $2/*
+cp -a $1/dist $2/
