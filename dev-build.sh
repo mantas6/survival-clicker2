c8cce81f7e43e2a95985306ac93aad10f06574d2
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-08-26 16:06:11 +0300

    Added automatic build lock

--- dev-build.sh
+++ dev-build.sh
@@ -7,6 +7,17 @@
 
 cd $1
 
+if [ -f build-lock ]; then
+  build_pid=`cat build-lock`
+
+  if ps -p $build_pid; then
+    echo "Process is still running"
+    exit 1;
+  fi
+fi
+
+echo $$ > build-lock
+
 changed=0
 git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
 
@@ -28,3 +39,5 @@ if [ $changed = 1 ]; then
 else
     echo "Up-to-date"
 fi
+
+rm build-lock
