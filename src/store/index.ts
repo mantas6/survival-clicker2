1e822316e610e8aa1c25cac881e322f21fa6346e
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-09-01 11:25:36 +0300

    Separate store files

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import { state } from './state';
+import { getters } from './getters';
+import { actions } from './actions';
+import { mutations } from './mutations';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state,
+  getters,
+  actions,
+  mutations,
+  strict: process.env.NODE_ENV === 'production',
 });
