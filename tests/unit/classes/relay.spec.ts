0babc788eaad28f171a61dedadf2da8b140e8ba5
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-09-01 12:16:26 +0300

    Add relay class test

---
+++ tests/unit/classes/relay.spec.ts
@@ -0,0 +1,35 @@
+import { expect, assert } from 'chai';
+import { Relay, RelayPayload } from '@/classes/relay';
+
+interface MessageEvent {
+  data: any;
+}
+
+class StubWorker {
+  public onmessage?: (event: MessageEvent) => void;
+
+  public postMessage(data: any) {
+    return;
+  }
+
+  public triggerMessage(data: RelayPayload) {
+    if (this.onmessage) {
+      this.onmessage({ data });
+    }
+  }
+}
+
+describe('classes/relay', () => {
+  it('delivers a message', (done) => {
+    // expect(Relay).to.exist();
+    const worker = new StubWorker();
+    const relay = new Relay(worker as any);
+
+    relay.on('testMessage', (data) => {
+      expect(data).to.equals('testPayload');
+      done();
+    });
+
+    worker.triggerMessage({ name: 'testMessage', data: 'testPayload' });
+  });
+});
