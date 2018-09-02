4490bcfa0b3814003be7f274a54bb093e98949e0
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-09-02 14:45:41 +0300

    Add finance

---
+++ src/classes/game/state/finance.ts
@@ -0,0 +1,5 @@
+import { Stat, Container } from '@/classes/game/stat';
+
+export class Finance {
+  public money = new Stat(0);
+}
--- src/classes/game/state/index.ts
+++ src/classes/game/state/index.ts
@@ -1,6 +1,8 @@
 import { Stat } from '@/classes/game/stat';
-import { Character } from '@/classes/game/state/character';
+import { Character } from './character';
+import { Finance } from './finance';
 
 export class State {
   public character = new Character();
+  public finance = new Finance();
 }
