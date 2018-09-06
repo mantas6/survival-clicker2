e162bc0c98dc1f539954015cd8d2dc212432efea
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-09-06 10:21:24 +0300

    Add mixin decorator

---
+++ src/utils/apply-mixins.ts
@@ -0,0 +1,18 @@
+interface WithPrototype {
+    prototype: any;
+}
+
+/**
+ * A decorator to apply mixins to a class
+ * @see https://www.typescriptlang.org/docs/handbook/mixins.html
+ * @param baseCtors Classes to implement to a base class
+ */
+export function applyMixins(baseCtors: WithPrototype[]) {
+    return (derivedCtor: WithPrototype) => {
+        baseCtors.forEach(baseCtor => {
+            Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+                derivedCtor.prototype[name] = baseCtor.prototype[name];
+            });
+        });
+    };
+}
