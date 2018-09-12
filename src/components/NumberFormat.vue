9c5ee2ff9f6a0c179a3b641809828a05b5d08a5f
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-09-12 13:31:00 +0300

    Add basic number format component

---
+++ src/components/NumberFormat.vue
@@ -0,0 +1,20 @@
+<template>
+  <span>{{ formatted }}</span>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Decimal from 'decimal.js';
+import { Formatter } from 'swarm-numberformat';
+
+@Component
+export default class NumberFormat extends Vue {
+  @Prop() private number!: Decimal;
+
+  formatter = new Formatter();
+
+  get formatted(): string {
+    return this.formatter.format(this.number);
+  }
+}
+</script>
