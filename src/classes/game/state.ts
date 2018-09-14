81c0a9c370264956b6aeb556ecd6150e1898d1a5
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-09-14 10:10:21 +0300

    Fix naming

--- src/classes/game/state.ts
+++ src/classes/game/state.ts
@@ -3,6 +3,6 @@ import { Processes } from '@/classes/game/processes';
 import { StateNode } from '@/classes/game/base/state-node';
 
 export class State extends StateNode {
-  public state = new Stats();
+  public stats = new Stats();
   public processes = new Processes();
 }
