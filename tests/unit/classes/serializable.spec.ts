a1ffb3b140775f2e7e92e990e3d5c598c447e310
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-09-18 11:45:04 +0300

    Update serializable test

--- tests/unit/classes/serializable.spec.ts
+++ tests/unit/classes/serializable.spec.ts
@@ -1,13 +1,33 @@
-import { Serializable } from '@/classes/game/base/serialization/serializable';
+/* tslint:disable:no-unused-expression */
+/* tslint:disable:max-classes-per-file */
+
+import { Serializable, Tag, SerializeAs } from '@/classes/game/base/serialization';
 import { expect } from 'chai';
 
-class MyClass extends Serializable {
-
+class ChildClass extends Serializable {
+  @Tag('emit')
+  @SerializeAs((input: string) => input)
+  public someText: string = 'someValue';
 }
 
-describe('test serializable class', () => {
-  it('exists', () => {
-    const ser = new MyClass();
+class ParentClass extends Serializable {
+  @Tag('emit')
+  public someProperty = new ChildClass();
+}
+
+describe('serializable.ts', () => {
+  const ser = new ParentClass();
+
+  it('serializes', () => {
     expect(ser).has.property('serialize');
+    const serialized = ser.serialize('emit');
+    expect(serialized).to.be.not.null;
+    expect(serialized).has.property('someProperty');
+  });
+
+  it('serializes children property', () => {
+    const serialized = ser.serialize('emit');
+    expect(serialized.someProperty).property('someText').exist;
+    expect(serialized.someProperty).property('someText').equals('someValue');
   });
 });
