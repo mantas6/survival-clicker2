f1a3b3a7b60c6d5b2a4081dc0313d67919c355c4
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-09-18 08:16:22 +0300

    Add test for NumberFormat

---
+++ tests/unit/components/number-format.spec.ts
@@ -0,0 +1,25 @@
+import { shallowMount } from '@vue/test-utils';
+import { expect } from 'chai';
+import NumberFormat from '@/components/NumberFormat.vue';
+
+
+describe('NumberFormat.vue', () => {
+  it('renders value when passed', () => {
+    const value = '1000';
+    const wrapper = shallowMount(NumberFormat, {
+      propsData: { value },
+    });
+
+    expect(wrapper.text()).to.include('1,000');
+  });
+
+  it('renders postfix when passed', () => {
+    const value = '1000';
+    const postFix = '$';
+    const wrapper = shallowMount(NumberFormat, {
+      propsData: { value, postFix },
+    });
+
+    expect(wrapper.text()).to.include('1,000$');
+  });
+});
