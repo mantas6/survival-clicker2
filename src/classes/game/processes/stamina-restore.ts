b9237d98e98b59e7962225d3e5f76eba4283cf62
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-09-20 11:40:40 +0300

    Remove comment

--- src/classes/game/processes/stamina-restore.ts
+++ src/classes/game/processes/stamina-restore.ts
@@ -3,7 +3,6 @@ import { Effect } from '@/classes/game/base/processes/effect';
 import Decimal from 'decimal.js';
 
 export class StaminaRestore extends Process {
-  // First argument needs to have reference to stats (TODO)
   public drain = new Effect(this.stats.character.health, () => {
     return new Decimal(1);
   });
