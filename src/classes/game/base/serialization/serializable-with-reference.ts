cfc9f4028bc41aee4acbb286a835db68ffa9107b
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-09-22 15:24:47 +0300

    Fix implementation error

--- src/classes/game/base/serialization/serializable-with-reference.ts
+++ src/classes/game/base/serialization/serializable-with-reference.ts
@@ -2,9 +2,10 @@ import { StateReference } from '@/classes/game/base/state-reference';
 import { State } from '@/classes/game/state';
 import { Stats } from '@/classes/game/stats';
 import { Implements } from '@/utils/implements';
+import { Serializable } from '@/classes/game/base/serialization/serializable';
 
 @Implements(StateReference)
-export class SerializableWithReference extends StateReference implements StateReference {
+export class SerializableWithReference extends Serializable implements StateReference {
   public state!: State;
   public stats!: Stats;
 }
