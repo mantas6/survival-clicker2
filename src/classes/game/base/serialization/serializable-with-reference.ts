e618cf703de81ffab982b3439e3da938efc184c1
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-09-22 15:56:55 +0300

    Move all methods to SerializableWithReference class from implemented one

--- src/classes/game/base/serialization/serializable-with-reference.ts
+++ src/classes/game/base/serialization/serializable-with-reference.ts
@@ -1,11 +1,13 @@
-import { StateReference } from '@/classes/game/base/state-reference';
 import { State } from '@/classes/game/state';
 import { Stats } from '@/classes/game/stats';
-import { Implements } from '@/utils/implements';
 import { Serializable } from '@/classes/game/base/serialization/serializable';
 
-@Implements(StateReference)
-export class SerializableWithReference extends Serializable implements StateReference {
-  public state!: State;
-  public stats!: Stats;
+export abstract class SerializableWithReference extends Serializable {
+  get state(): State {
+    return this.root as State;
+  }
+
+  get stats(): Stats {
+    return this.state.stats;
+  }
 }
