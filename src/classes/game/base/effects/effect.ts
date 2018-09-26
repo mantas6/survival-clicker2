4dd5afefae0d1d75a4bf7c58ac280e5e05f9ba0d
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-09-26 07:50:11 +0300

    Mock-up implementatino of effect descriptor system

--- src/classes/game/base/effects/effect.ts
+++ src/classes/game/base/effects/effect.ts
@@ -1,5 +1,14 @@
 import { SerializableWithReference } from '@/classes/game/base/serialization';
 
+interface PropertyDescriptor {
+  group: number;
+}
+
 export class Effect extends SerializableWithReference {
+  // public static descriptorsOfProperties = new Map<string, PropertyDescriptor>();
+  // public 'constructor': typeof Effect;
+
+  // Time that this effect is active for
+  public elapsed: number = 0;
 
 }
