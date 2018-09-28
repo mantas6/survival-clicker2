e72bd10c8135961f05d79e6147af4a2dbd3a7346
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-09-28 11:21:22 +0300

    Prototype effect

--- src/classes/game/base/effects/effect.ts
+++ src/classes/game/base/effects/effect.ts
@@ -1,14 +1,17 @@
 import { SerializableWithReference } from '@/classes/game/base/serialization';
+import { Decimal } from 'decimal.js';
 
-interface PropertyDescriptor {
-  group: number;
+export interface PropertyDescriptor {
+  groupIndex?: number;
+  durationFunc?: () => Decimal;
 }
 
+export type PropertyDescriptorMap = Map<string, PropertyDescriptor>;
+
 export class Effect extends SerializableWithReference {
-  // public static descriptorsOfEffects = new Map<string, PropertyDescriptor>();
+  public static descriptorsOfMutations: PropertyDescriptorMap = new Map();
   public 'constructor': typeof Effect;
 
   // Time that this effect is active for
   public elapsed: number = 0;
-
 }
---
+++ src/classes/game/base/effects/index.ts
@@ -0,0 +1,8 @@
+import { Effect } from './effect';
+import { Duration, Group } from './decorators';
+
+export {
+  Effect,
+  Duration,
+  Group,
+};
