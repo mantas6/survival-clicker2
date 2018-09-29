04bd48c44ad39d7388788012da0e45c1ea98d4a8
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-09-29 15:27:19 +0300

    Remove boostrap-vue

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <b-container>
+    <div>
       <router-view/>
-    </b-container>
+    </div>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import '@babel/polyfill';
 import Vue from 'vue';
-import './plugins/bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from '@/store';
