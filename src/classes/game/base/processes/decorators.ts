7faa527e98c2d3bd96d0721ad03ca3d0b9f759f1
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-10-02 08:09:00 +0300

    Add duration decorator for effect

--- src/classes/game/base/processes/decorators.ts
+++ src/classes/game/base/processes/decorators.ts
@@ -1,5 +1,32 @@
-import { ProcessType, Process } from './process';
+import { ProcessType, Process, EffectDescriptorMap } from './process';
+import Decimal from 'decimal.js';
 
 export function Auto(processClass: Process) {
   processClass.constructor.type = ProcessType.Auto;
+}
+
+export function Duration(duration: Decimal) {
+  return (processClass: Process, propertyName: string) => {
+    const descriptors = prepareDescriptorsOfProperty(processClass, propertyName);
+    const descriptor = descriptors.get(propertyName);
+
+    if (descriptor) {
+      descriptor.duration = duration;
+    }
+  };
+}
+
+function prepareDescriptorsOfProperty(processClass: Process, propertyName: string): EffectDescriptorMap {
+  const ctor = processClass.constructor;
+  // Copying the variable so that it doesn't mutate the prototype class
+  const descriptors = new Map(ctor.descriptorsOfEffects);
+
+  if (!descriptors.has(propertyName)) {
+    descriptors.set(propertyName, {});
+  }
+
+  // Forwarding the copy to the class
+  ctor.descriptorsOfEffects = descriptors;
+
+  return descriptors;
 }
--- src/classes/game/base/processes/process.ts
+++ src/classes/game/base/processes/process.ts
@@ -1,14 +1,22 @@
 import { Calculable } from '@/classes/game/base/processes/effect';
 import { SerializableWithReference } from '@/classes/game/base/serialization';
 import { Effect } from './effect';
+import Decimal from 'decimal.js';
 
 export enum ProcessType {
     Manual = 'manual',
     Auto = 'auto',
 }
 
+export interface EffectDescriptor {
+  duration?: Decimal;
+}
+
+export type EffectDescriptorMap = Map<string, EffectDescriptor>;
+
 export abstract class Process extends SerializableWithReference {
   public static type: ProcessType = ProcessType.Manual;
+  public static descriptorsOfEffects: EffectDescriptorMap = new Map();
   public 'constructor': typeof Process;
 
   public validate(): boolean {
