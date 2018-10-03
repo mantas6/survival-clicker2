cf88a6f7fa566c4ae15279984c02063f828bbe6f
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-10-03 07:49:24 +0300

    Add the actual exports

--- src/classes/game/base/processes/index.ts
+++ src/classes/game/base/processes/index.ts
@@ -1,3 +1,10 @@
 import { Auto, Duration } from './decorators';
 import { Process } from './process';
 import { Effect } from './effect';
+
+export {
+  Auto,
+  Duration,
+  Process,
+  Effect,
+};
