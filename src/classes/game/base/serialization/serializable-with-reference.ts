663ef73cabb0eddd3315a83fcff45e980baef269
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-10-04 08:00:52 +0300

    Add reminder commend

--- src/classes/game/base/serialization/serializable-with-reference.ts
+++ src/classes/game/base/serialization/serializable-with-reference.ts
@@ -4,6 +4,7 @@ import { Serializable } from '@/classes/game/base/serialization/serializable';
 import { NonChild } from '@/classes/game/base/state-node';
 
 export abstract class SerializableWithReference extends Serializable {
+  // Do we really need these NoChild exclusions, since it is not an instanceof StateNode
   @NonChild
   get state(): State {
     return this.root as State;
