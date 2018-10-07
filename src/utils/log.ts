96294fe252999d7f907a3470e7bddf8a927408f4
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-07 10:29:05 +0300

    Add a rudamentary stack trace support

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -4,10 +4,28 @@ let isEnabled = false;
 
 export function log(...data: any[]) {
   if (isEnabled) {
-    console.log(...data);
+    const message = format_message(data);
+
+    console.log(...message);
   }
+}
+
+function format_message(data: any[]) {
+  const error = new Error();
+
+  let name = '';
+
+  if (error.stack) {
+    const stack = error.stack.split('\n')
+      .map(line => line.trim());
+
+    name = stack[3];
+  }
+
+  return [ name, ...data ];
 }
 
 export function enableLogging() {
   isEnabled = true;
+  log('Debugging mode is enabled');
 }
