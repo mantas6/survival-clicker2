db352e1e50c7b2678da8a071d0e5aa1d35d0d784
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-10 15:27:20 +0300

    Restruct routing

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Actions from './views/Actions.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: '/actions/:name',
+      name: 'actions',
+      component: Actions,
     },
   ],
 });
---
+++ src/views/Actions.vue
@@ -0,0 +1,19 @@
+<template>
+  <article>
+    
+  </article>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { mapGetters } from 'vuex';
+
+@Component({
+  computed: {
+    ...mapGetters(['processes', 'stats']),
+  },
+})
+export default class Actions extends Vue {
+  
+}
+</script>
