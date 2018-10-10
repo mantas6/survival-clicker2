35e3d3765774910ac7813659c407431e20ff6da2
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-10 14:59:02 +0300

    Add money display

--- src/components/header/HeaderContainer.vue
+++ src/components/header/HeaderContainer.vue
@@ -1,18 +1,31 @@
 <template>
   <header>
     <game-logo></game-logo>
+    <number-format :value="money"></number-format>
   </header>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import GameLogo from './GameLogo.vue';
+import { mapGetters } from 'vuex';
+import { Stats } from '@/store/stats';
+import { SerializedMap } from '@/store/types';
 
 @Component({
   components: { GameLogo },
+  computed: { ...mapGetters(['stats']) },
 })
 export default class HeaderContainer extends Vue {
+  stats!: SerializedMap;
 
+  get money() {
+    if (this.stats.finance) {
+      return this.stats.finance.money.current;
+    }
+
+    return 0;
+  }
 }
 </script>
 
