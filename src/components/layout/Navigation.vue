745de4cb83cd70710aa4facc82325a4f25d16c1e
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-12 16:01:05 +0300

    Add missing component declaration

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -5,6 +5,16 @@
   </nav>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Navigation extends Vue {
+  
+}
+</script>
+
+
 <style lang="scss" scoped>
   nav {
     margin-bottom: 4rem;
