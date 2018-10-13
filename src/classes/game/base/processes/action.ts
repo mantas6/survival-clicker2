93e78cf3049ce8a7565e9fbdebf547328e593015
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-13 09:53:50 +0300

    Setup action to emit it's absolute path Here is where I start to really notice one issue: property/method name collisions in the configuration classes. While it might seem not a big deal right now, in the future I see this becoming a great deal of mess, since more and more names are getting 'reserved'. This leads to variable names being less intuitive and way less oblivous since I have to 'workaround' the collision every time, making this note so that I can remember to comeback to this once I have a basic game functionality going, then I need to address this seriously.

--- src/classes/game/base/processes/action.ts
+++ src/classes/game/base/processes/action.ts
@@ -1,5 +1,11 @@
 import { Process, ProcessType } from './process';
+import { SerializeOn } from '@/classes/game/base/serialization';
 
 export class Action extends Process {
   public static type: ProcessType = ProcessType.Manual;
+
+  @SerializeOn('emit')
+  get actionName() {
+    return this.path;
+  }
 }
