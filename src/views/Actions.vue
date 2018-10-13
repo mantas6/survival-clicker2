f56f5477e7a3bf6e4f4b2deb27e82f73184914c5
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-13 09:35:04 +0300

    Implement actions view

--- src/views/Actions.vue
+++ src/views/Actions.vue
@@ -1,6 +1,11 @@
 <template>
   <article>
-    
+    <section>
+      <div v-for="(action, actionName) of availableActions" :key="actionName" class="item">
+        <span class="name">{{ actionName }}</span>
+        <number-format class="cost" v-if="action.money" :value="action.money.diff" post-fix="$"></number-format>
+      </div>
+    </section>
   </article>
 </template>
 
@@ -9,7 +14,35 @@ import { Component, Vue } from 'vue-property-decorator';
 import { Getter } from 'vuex-class';
 import { SerializedActions } from '@/store/actions';
 
+@Component
 export default class Actions extends Vue {
-  @Getter public actions!: SerializedActions;
+  @Getter public processes!: SerializedActions;
+
+  get availableActions() {
+    if (!this.processes) return;
+    const category = this.$route.params.name as 'jobs' | 'consumables';
+
+    return this.processes[category];
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  article {
+    flex: 1;
+  }
+
+  section {
+    display: grid;
+    grid-gap: 1rem;
+
+    .item {
+      display: flex;
+      justify-content: space-between;
+      width: 50%;
+      padding: 0.75rem;
+      padding-left: 0;
+      text-transform: capitalize;
+    }
+  }
+</style>
