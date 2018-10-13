b3a34dbc4383aaf676470c72af5f4de7e15696f7
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-13 13:01:47 +0300

    Setup processes container

--- src/classes/game/processes/index.ts
+++ src/classes/game/processes/index.ts
@@ -1,8 +1,8 @@
 import { StaminaRestore } from '@/classes/game/processes/stamina-restore';
-import { Serializable } from '@/classes/game/base/serialization/serializable';
 import { SerializeOn } from '@/classes/game/base/serialization';
+import { ProcessContainer } from '@/classes/game/base/processes';
 
-export class Processes extends Serializable {
+export class Processes extends ProcessContainer {
   @SerializeOn('emit')
   public staminaRestore = new StaminaRestore();
 }
