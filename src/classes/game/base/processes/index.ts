e012aff059a34ff7fd79a1d61d0a0a74e6d1072e
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-13 13:01:20 +0300

    Implement process container for nested calculation

--- src/classes/game/base/processes/index.ts
+++ src/classes/game/base/processes/index.ts
@@ -1,10 +1,12 @@
 import { Auto, Duration } from './decorators';
 import { Process } from './process';
 import { Action } from './action';
+import { ProcessContainer } from './process-container';
 
 export {
   Auto,
   Duration,
   Process,
   Action,
+  ProcessContainer,
 };
---
+++ src/classes/game/base/processes/process-container.ts
@@ -0,0 +1,12 @@
+import { Serializable } from '@/classes/game/base/serialization';
+import { Process } from './process';
+import { Calculable } from '@/classes/game/base/effects';
+
+export class ProcessContainer extends Serializable implements Calculable {
+  public calculate() {
+    const filter = (entry: Calculable) => entry instanceof ProcessContainer || entry instanceof Process;
+    for (const processChild of this.children(filter)) {
+      processChild.calculate();
+    }
+  }
+}
