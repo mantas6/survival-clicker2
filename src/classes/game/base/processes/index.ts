d2f67afbd393f0b2ff164bd4cbb1c21c0de24781
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-13 09:50:26 +0300

    Add action re-export

--- src/classes/game/base/processes/index.ts
+++ src/classes/game/base/processes/index.ts
@@ -1,8 +1,10 @@
 import { Auto, Duration } from './decorators';
 import { Process } from './process';
+import { Action } from './action';
 
 export {
   Auto,
   Duration,
   Process,
+  Action,
 };
