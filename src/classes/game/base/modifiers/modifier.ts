0d5fabc739bb66315402b3a28bf7a8e4cc152d6d
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-19 11:30:24 +0300

    Make modifiers serializable

--- src/classes/game/base/modifiers/modifier.ts
+++ src/classes/game/base/modifiers/modifier.ts
@@ -1,12 +1,15 @@
 import Decimal from 'decimal.js';
+import { Serializable, SerializeOn } from '@/classes/game/base/serialization';
 
-export class Modifier {
+export class Modifier extends Serializable {
   private computeFunc: () => Decimal;
 
   constructor(computeFunc: () => Decimal) {
+    super();
     this.computeFunc = computeFunc;
   }
 
+  @SerializeOn('emit')
   get value() {
     return this.computeFunc();
   }
