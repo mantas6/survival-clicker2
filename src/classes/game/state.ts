0148b523bcbfa167a1fb97397c3cf3d17285a75b
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-20 13:38:26 +0300

    Add timers to state

--- src/classes/game/state.ts
+++ src/classes/game/state.ts
@@ -3,12 +3,14 @@ import { Processes } from '@/classes/game/processes';
 import { Actions } from '@/classes/game/actions';
 import { Modifiers } from '@/classes/game/modifiers';
 import { StateRoot } from '@/classes/game/base/state-root';
+import { Timers } from './base/effects';
 
 export class State extends StateRoot {
   stats = new Stats();
   processes = new Processes();
   actions = new Actions();
   modifiers = new Modifiers();
+  timers = new Timers();
 
   // For the time being
   constructor() {
