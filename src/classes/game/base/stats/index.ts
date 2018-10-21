e258c527cae4c80d71a3ff263e67e4c85d41d391
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-21 10:56:17 +0300

    Add a comment block

--- src/classes/game/base/stats/index.ts
+++ src/classes/game/base/stats/index.ts
@@ -1,6 +1,15 @@
 import { Value } from './value';
 import { Container } from './container';
 
+/**
+ * Describes a limit that is reached after stat mutation
+ *
+ * true - no limit is reached
+ *
+ * lessThanMinimum - stat was floored to it's minimum value
+ *
+ * greaterThanMaximum - stat was ceiled to it's maximum value
+ */
 export type LimitFlag = true | 'lessThanMinimum' | 'greaterThanMaximum';
 
 export { Value, Container };
