9c6a4b11a18bbfd8aa7acac13d7783fcb7c59b26
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-26 11:36:02 +0300

    Use Duration on consumables (temporary)

--- src/classes/game/actions/consumables/cheap-food.ts
+++ src/classes/game/actions/consumables/cheap-food.ts
@@ -1,9 +1,10 @@
-import { Action } from '@/classes/game/base/processes';
+import { Action, Duration } from '@/classes/game/base/processes';
 import { Effect } from '@/classes/game/base/effects';
 import { SerializeAllOn } from '@/classes/game/base/serialization';
 
 @SerializeAllOn('emit')
 export class CheapFood extends Action {
+  @Duration(() => 5)
   stamina = new Effect(() => this.stats.character.energy, value => {
     return value.add(1);
   });
--- src/classes/game/actions/consumables/cheap-water.ts
+++ src/classes/game/actions/consumables/cheap-water.ts
@@ -1,9 +1,10 @@
-import { Action } from '@/classes/game/base/processes';
+import { Action, Duration } from '@/classes/game/base/processes';
 import { Effect } from '@/classes/game/base/effects';
 import { SerializeAllOn } from '@/classes/game/base/serialization';
 
 @SerializeAllOn('emit')
 export class CheapWater extends Action {
+  @Duration(() => 5)
   hydration = new Effect(() => this.stats.character.hydration, value => {
     return value.add(1);
   });
