b2299ad418f50693c65ed061f25a81d2c195e8b4
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-27 12:03:14 +0300

    Allow stomach to digest fully

--- src/classes/game/processes/character/digestion.ts
+++ src/classes/game/processes/character/digestion.ts
@@ -1,7 +1,9 @@
 import { Process } from '@/classes/game/base/processes/process';
 import { Effect } from '@/classes/game/base/effects';
+import { IgnoreLimits } from '@/classes/game/base/processes';
 
 export class Digestion extends Process {
+  @IgnoreLimits('lessThanMinimum')
   drainStomach = new Effect(() => this.stats.character.stomach, value => {
     return value.sub(this.modifiers.character.digestionSpeed.value);
   });
