2697b8b7aa5f5ce960e0c992ec5fcf000093cc20
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-10-28 14:25:19 +0200

    Enable reset for action unlocking Temporary solution only

--- src/classes/game/base/stats/methods.ts
+++ src/classes/game/base/stats/methods.ts
@@ -1,11 +1,15 @@
 import { Value, Container } from '.';
 import { State } from '@/classes/game/state';
 import { apply } from '@/utils/node';
+import { Action } from '../processes';
 
 export function applyReset(state: State) {
-  apply<Value | Container>(state, node => {
+  apply<Value | Container | Action>(state, node => {
+    // Need to setup decorators for global reset() functionality that will reset the actual storable variables
     if (node instanceof Value) {
       node.current = undefined;
+    } else if (node instanceof Action) {
+      node.isUnlocked = false;
     }
   });
 }
