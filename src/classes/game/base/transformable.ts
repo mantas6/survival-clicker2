4f35918dbc86fbd7861dca98fdb352062f5fca3e
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-10-30 10:26:05 +0200

    Prototype transformable class

---
+++ src/classes/game/base/transformable.ts
@@ -0,0 +1,29 @@
+export interface TransformationDescriptor {
+  tagName: string;
+  propertyName: string;
+  valueFunc: () => any;
+  conditionFunc: () => boolean;
+}
+
+export function Transform<Target>(tagName: string, valueFunc: () => Target, conditionFunc: () => boolean) {
+  return (transformableClass: Transformable, propertyName: string) => {
+    const ctor = transformableClass.constructor;
+
+    const transformation = { tagName, propertyName, valueFunc, conditionFunc };
+
+    ctor.transformations = [ ...ctor.transformations, transformation ];
+  };
+}
+
+export class Transformable {
+  static transformations: TransformationDescriptor[] = [];
+  'constructor': typeof Transformable;
+
+  transform(tagName: string) {
+    for (const transformation of this.constructor.transformations) {
+      if (transformation.tagName === tagName && transformation.conditionFunc()) {
+        (this as any)[transformation.propertyName] = transformation.valueFunc();
+      }
+    }
+  }
+}
