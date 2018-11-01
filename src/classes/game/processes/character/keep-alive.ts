c4d8359692e672cffde402f9dbcae968bf9c5095
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-01 10:43:47 +0200

    Set keep-alive to ignore limits

--- src/classes/game/processes/character/keep-alive.ts
+++ src/classes/game/processes/character/keep-alive.ts
@@ -1,12 +1,14 @@
-import { Process, When } from '@/classes/game/base/processes';
+import { Process, IgnoreLimits } from '@/classes/game/base/processes';
 import { Effect } from '@/classes/game/base/effects';
 import Decimal from 'decimal.js';
 
 export class KeepAlive extends Process {
+  @IgnoreLimits('lessThanMinimum')
   drainEnergy = new Effect(() => this.stats.character.energy, () => {
     return new Decimal(-0.5);
   });
 
+  @IgnoreLimits('lessThanMinimum')
   drainHydration = new Effect(() => this.stats.character.hydration, () => {
     return new Decimal(-0.5);
   });
