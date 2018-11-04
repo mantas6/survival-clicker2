b978c786e2b5756dae09ca7445e4eb3310254192
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-04 12:18:42 +0200

    Set stomach starting value to 0

--- src/classes/game/stats/character/stomach.ts
+++ src/classes/game/stats/character/stomach.ts
@@ -2,7 +2,7 @@ import { Container } from '@/classes/game/base/stats';
 import Decimal from 'decimal.js';
 
 export class Stomach extends Container {
-  readonly default = 100;
+  readonly default = 0;
 
   get maximum() {
     return new Decimal(100);
