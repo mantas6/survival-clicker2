f67b79b97c1607bc675af5640912b3964ed6e258
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-11-07 10:01:32 +0200

    Also ignore Effects

--- tests/unit/classes/state.spec.ts
+++ tests/unit/classes/state.spec.ts
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import { State } from '@/classes/game/state';
 import { StateNode } from '@/classes/game/base/state-node';
 import { Mutation } from '@/classes/game/base/mutations';
-import { Modifier } from '@/classes/game/base/modifiers';
+import { Modifier, Effect } from '@/classes/game/base/modifiers';
 
 describe('Game State', function() {
   it('does not have an property/class name mismatches', function() {
@@ -14,7 +14,7 @@ describe('Game State', function() {
 
     function check(parent: StateNode) {
       for (const { name, node } of parent.children<StateNode>(entry => entry instanceof StateNode)) {
-        if (node instanceof Mutation || node instanceof Modifier) {
+        if (node instanceof Mutation || node instanceof Effect || node instanceof Modifier) {
           continue;
         }
 
