458784e2b452109c61bb89a98218401eb404de2e
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-11-07 08:21:33 +0200

    Remove condition function for transformable

--- src/classes/game/base/transformable.ts
+++ src/classes/game/base/transformable.ts
@@ -1,29 +1,29 @@
 import { SerializableWithReference } from '@/classes/game/base/serialization';
+import { State } from '@/classes/game/state';
 
 export interface TransformationDescriptor {
   tagName: string;
   propertyName: string;
   valueFunc: () => any;
-  conditionFunc: () => boolean;
 }
 
-export function Transform<Target>(tagName: string, valueFunc: () => Target, conditionFunc: () => boolean) {
+export function Transform<Target>(tagName: string, valueFunc: () => Target) {
   return (transformableClass: Transformable, propertyName: string) => {
     const ctor = transformableClass.constructor;
 
-    const transformation = { tagName, propertyName, valueFunc, conditionFunc };
+    const transformation = { tagName, propertyName, valueFunc };
 
     ctor.transformations = [ ...ctor.transformations, transformation ];
   };
 }
 
-export class Transformable extends SerializableWithReference {
+export abstract class Transformable extends SerializableWithReference {
   static transformations: TransformationDescriptor[] = [];
   'constructor': typeof Transformable;
 
   transform(tagName: string) {
     for (const transformation of this.constructor.transformations) {
-      if (transformation.tagName === tagName && transformation.conditionFunc()) {
+      if (transformation.tagName === tagName) {
         (this as any)[transformation.propertyName] = transformation.valueFunc();
       }
     }
