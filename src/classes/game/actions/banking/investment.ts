eb636b9f8be76e41163da0129fa3009970df18a8
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-09 12:17:03 +0200

    Make investment locked by default

--- src/classes/game/actions/banking/investment.ts
+++ src/classes/game/actions/banking/investment.ts
@@ -1,10 +1,11 @@
-import { Action } from '@/classes/game/base/actions';
+import { Action, Unlocks } from '@/classes/game/base/actions';
 import { Mutation } from '@/classes/game/base/mutations';
 import { SerializeAllOn } from '@/classes/game/base/serialization';
 import Decimal from 'decimal.js';
 
 @SerializeAllOn('emit')
 export class Investment extends Action {
+  @Unlocks
   money = new Mutation(() => this.stats.finance.money, () => {
     const base = this.stats.finance.investment.value.add(10);
     return this.modifiers.finance.costAdd.value.mul(base).ceil().negated();
