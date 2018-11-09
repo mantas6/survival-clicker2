2277743b24cb7b51ad63286206ab09f841de3573
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-09 09:55:24 +0200

    Fix apply

--- src/utils/node.ts
+++ src/utils/node.ts
@@ -1,7 +1,7 @@
 import { StateNode } from '@/classes/game/base/state-node';
 
 export function apply<TargetNode extends StateNode>(parentNode: StateNode, applyFunc: (node: TargetNode) => void) {
-  for (const node of parentNode.nodes()) {
+  for (const { node } of parentNode.nodes()) {
     applyFunc(node as TargetNode);
     apply(node, applyFunc);
   }
