3ded6d96bd42b26b22e98d7e8536d495edff328f
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-10 14:08:56 +0200

    Fix processing check isPos returns true when value is 0

--- src/classes/game/processes/character/intake/energy.ts
+++ src/classes/game/processes/character/intake/energy.ts
@@ -2,7 +2,7 @@ import { Process, When } from '@/classes/game/base/processes';
 import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
 
-@When(process => process.modifiers.character.intake.energy.value.isPos())
+@When(process => process.modifiers.character.intake.energy.value.greaterThan(0))
 export class Energy extends Process {
   restoreEnergy = new Mutation(() => this.stats.character.energy, () => {
     const cumulated = this.modifiers.character.intake.energy.value;
--- src/classes/game/processes/character/intake/hydration.ts
+++ src/classes/game/processes/character/intake/hydration.ts
@@ -2,7 +2,7 @@ import { Process, When } from '@/classes/game/base/processes';
 import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
 
-@When(process => process.modifiers.character.intake.hydration.value.isPos())
+@When(process => process.modifiers.character.intake.hydration.value.greaterThan(0))
 export class Hydration extends Process {
   restoreHydration = new Mutation(() => this.stats.character.hydration, () => {
     const cumulated = this.modifiers.character.intake.hydration.value;
