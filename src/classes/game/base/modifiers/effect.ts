c84f315721a0c5871d4c4f7ff355228be3422d79
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-11 13:40:43 +0200

    Setup effect serialization

--- src/classes/game/base/modifiers/effect.ts
+++ src/classes/game/base/modifiers/effect.ts
@@ -1,4 +1,4 @@
-import { SerializableWithReference } from '@/classes/game/base/serialization';
+import { SerializableWithReference, SerializeOn, SerializeAs } from '@/classes/game/base/serialization';
 import Decimal from 'decimal.js';
 import { Modifier } from '.';
 import { CalculationOptions } from '../mutations';
@@ -15,8 +15,16 @@ export class Effect extends SerializableWithReference {
     this.modifierFunc = modifierFunc;
   }
 
+  @SerializeOn('emit')
+  @SerializeAs<Modifier>(input => input.path)
   get modifier() {
     return this.modifierFunc();
+  }
+
+  @SerializeOn('emit')
+  get value() {
+    const multiplier = new Decimal(1);
+    return this.compute({ multiplier });
   }
 
   compute(opts: CalculationOptions): Decimal {
