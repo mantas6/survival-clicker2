68f8ef91c79c1fbd4356f45c65083a19e618a108
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-11-12 08:47:41 +0200

    Add energy drink

---
+++ src/classes/game/actions/drugs/stimulants/energy-drink.ts
@@ -0,0 +1,29 @@
+import { Action, Unlocks } from '@/classes/game/base/actions';
+import { Mutation } from '@/classes/game/base/mutations';
+import { SerializeAllOn } from '@/classes/game/base/serialization';
+import { Effect } from '@/classes/game/base/modifiers';
+import Decimal from 'decimal.js';
+
+@SerializeAllOn('emit')
+export class EnergyDrink extends Action {
+  drainEnergy = new Effect({
+    modifier: () => this.modifiers.character.intake.hydration,
+    duration: () => 60,
+    value: () => new Decimal(-0.1),
+  });
+
+  restoreStamina = new Effect({
+    modifier: () => this.modifiers.character.staminaRestoreSpeed,
+    duration: () => 5,
+    value: () => new Decimal(3),
+  });
+
+  stomach = new Mutation(() => this.stats.character.stomach, () => {
+    return new Decimal(2);
+  });
+
+  @Unlocks
+  money = new Mutation(() => this.stats.finance.money, () => {
+    return this.modifiers.finance.costAdd.value.mul(2).ceil().negated();
+  });
+}
--- src/classes/game/actions/drugs/stimulants/index.ts
+++ src/classes/game/actions/drugs/stimulants/index.ts
@@ -1,7 +1,9 @@
 import { Serializable, SerializeAllOn } from '@/classes/game/base/serialization';
 import { CheapStimulant } from './cheap-stimulant';
+import { EnergyDrink } from './energy-drink';
 
 @SerializeAllOn('emit', 'store')
 export class Stimulants extends Serializable {
+  energyDrink = new EnergyDrink();
   cheapStimulant = new CheapStimulant();
 }
