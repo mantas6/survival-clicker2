baec2303cbd54e8d4cdd35e7eb2ea4cc2d7319f7
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-11-15 08:52:00 +0200

    Add digestion speed limit

--- src/classes/game/processes/character/digestion.ts
+++ src/classes/game/processes/character/digestion.ts
@@ -1,10 +1,14 @@
 import { Process } from '@/classes/game/base/processes/process';
 import { Mutation } from '@/classes/game/base/mutations';
 import { IgnoreLimits } from '@/classes/game/base/processes';
+import Decimal from 'decimal.js';
 
 export class Digestion extends Process {
   @IgnoreLimits('lessThanMinimum')
   drainStomach = new Mutation(() => this.stats.character.stomach, () => {
-    return this.modifiers.character.digestionSpeed.value.negated();
+    const cumulated = this.modifiers.character.digestionSpeed.value;
+    const absorbLimit = new Decimal(5);
+
+    return Decimal.min(cumulated, absorbLimit).negated();
   });
 }
