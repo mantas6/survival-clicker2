954675a3d40e7c2d0d5788cc2c9330db9d9eaefb
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-16 15:08:35 +0200

    Add education requirements

--- src/classes/game/actions/drugs/stimulants/cheap-stimulant.ts
+++ src/classes/game/actions/drugs/stimulants/cheap-stimulant.ts
@@ -1,10 +1,11 @@
-import { Action, Unlocks } from '@/classes/game/base/actions';
+import { Action, Unlocks, UnlocksWhen } from '@/classes/game/base/actions';
 import { Mutation } from '@/classes/game/base/mutations';
 import { SerializeAllOn } from '@/classes/game/base/serialization';
 import { Effect } from '@/classes/game/base/modifiers';
 import Decimal from 'decimal.js';
 
 @SerializeAllOn('emit')
+@UnlocksWhen(action => action.stats.education.school.value.greaterThan(3))
 export class CheapStimulant extends Action {
   drainEnergy = new Effect({
     modifier: () => this.modifiers.character.intake.energy,
--- src/classes/game/actions/jobs/donor/sell-blood.ts
+++ src/classes/game/actions/jobs/donor/sell-blood.ts
@@ -1,9 +1,10 @@
-import { Action } from '@/classes/game/base/actions';
+import { Action, UnlocksWhen } from '@/classes/game/base/actions';
 import { Mutation } from '@/classes/game/base/mutations';
 import { SerializeAllOn } from '@/classes/game/base/serialization';
 import Decimal from 'decimal.js';
 
 @SerializeAllOn('emit')
+@UnlocksWhen(action => action.stats.education.school.value.greaterThan(2))
 export class SellBlood extends Action {
   money = new Mutation(() => this.stats.finance.money, () => {
     return new Decimal(100).times(this.stats.character.health.level).floor();
