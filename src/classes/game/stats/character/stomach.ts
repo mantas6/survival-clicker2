7dbe6260afd6143b6b7cceb7eb9ab02ef7001876
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-16 09:25:07 +0200

    Start at half full

--- src/classes/game/stats/character/stomach.ts
+++ src/classes/game/stats/character/stomach.ts
@@ -2,7 +2,7 @@ import { Container } from '@/classes/game/base/stats';
 import Decimal from 'decimal.js';
 
 export class Stomach extends Container {
-  readonly default = 0;
+  readonly default = 50;
 
   get maximum() {
     return new Decimal(100);
