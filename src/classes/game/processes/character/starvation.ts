ab4573e4d10079da227b98399c91273f79ec8ca8
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-16 09:38:47 +0200

    Fix whitespace

--- src/classes/game/processes/character/starvation.ts
+++ src/classes/game/processes/character/starvation.ts
@@ -2,7 +2,7 @@ import { Process, When } from '@/classes/game/base/processes';
 import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
 
-@When(process => process.stats.character.energy.value.isZero() ||  process.stats.character.stomach.value.isZero())
+@When(process => process.stats.character.energy.value.isZero() || process.stats.character.stomach.value.isZero())
 export class Starvation extends Process {
   drainHealth = new Mutation(() => this.stats.character.health, () => {
     return new Decimal(-0.1);
