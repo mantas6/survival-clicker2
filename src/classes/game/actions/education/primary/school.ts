abe9c2b928bf9129fc9ed38ec8bce0abfc75f2ca
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-16 15:45:54 +0200

    Fix ordering

--- src/classes/game/actions/education/primary/school.ts
+++ src/classes/game/actions/education/primary/school.ts
@@ -6,6 +6,13 @@ import Decimal from 'decimal.js';
 
 @SerializeAllOn('emit')
 export class School extends Action {
+  @Unlocks
+  money = new Mutation(() => this.stats.finance.money, () => {
+    const timesBought = this.stats.education.school.value;
+    const base = new Decimal(50).mul(new Decimal(1.5).pow(timesBought));
+    return this.modifiers.finance.costAdd.value.mul(base).ceil().negated();
+  });
+
   energy = new Effect({
     modifier: () => this.modifiers.character.intake.energy,
     duration: () => 30,
@@ -14,12 +21,5 @@ export class School extends Action {
 
   school = new Mutation(() => this.stats.education.school, () => {
     return new Decimal(1);
-  });
-
-  @Unlocks
-  money = new Mutation(() => this.stats.finance.money, () => {
-    const timesBought = this.stats.education.school.value;
-    const base = new Decimal(50).mul(new Decimal(1.5).pow(timesBought));
-    return this.modifiers.finance.costAdd.value.mul(base).ceil().negated();
   });
 }
