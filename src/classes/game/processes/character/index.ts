a711fd672a1398dedf6e766ee21ff1bf1a2e0793
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-16 09:43:38 +0200

    Add vomiting

--- src/classes/game/processes/character/index.ts
+++ src/classes/game/processes/character/index.ts
@@ -6,6 +6,7 @@ import { KeepAlive } from './keep-alive';
 import { Healing } from './healing';
 import { ProcessContainer } from '@/classes/game/base/processes';
 import { Intake } from './intake';
+import { Vomiting } from './vomiting';
 
 export class Character extends ProcessContainer {
   intake = new Intake();
@@ -15,4 +16,5 @@ export class Character extends ProcessContainer {
   starvation = new Starvation();
   keepAlive = new KeepAlive();
   healing = new Healing();
+  vomiting = new Vomiting();
 }
---
+++ src/classes/game/processes/character/vomiting.ts
@@ -0,0 +1,15 @@
+import { Process, When, IgnoreLimits } from '@/classes/game/base/processes';
+import { Mutation } from '@/classes/game/base/mutations';
+import Decimal from 'decimal.js';
+
+@When(process => process.stats.character.stomach.level.greaterThan(0.9))
+export class Vomiting extends Process {
+  @IgnoreLimits('lessThanMinimum')
+  health = new Mutation(() => this.stats.character.health, () => {
+    return new Decimal(-10);
+  });
+
+  stomach = new Mutation(() => this.stats.character.stomach, () => {
+    return this.stats.character.stomach.value.mul(0.5).negated();
+  });
+}
