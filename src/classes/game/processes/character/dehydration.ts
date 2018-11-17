700597bec608ef3e9348a2dff680f9fbb180feda
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-17 09:53:08 +0200

    Allow character to die

--- src/classes/game/processes/character/dehydration.ts
+++ src/classes/game/processes/character/dehydration.ts
@@ -1,9 +1,10 @@
-import { Process, When } from '@/classes/game/base/processes';
+import { Process, When, IgnoreLimits } from '@/classes/game/base/processes';
 import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
 
 @When(process => process.stats.character.hydration.value.isZero())
 export class Dehydration extends Process {
+  @IgnoreLimits('lessThanMinimum')
   drainHealth = new Mutation(() => this.stats.character.health, () => {
     return new Decimal(-3).div(this.modifiers.character.healthPreservationMultiplier.value);
   });
--- src/classes/game/processes/character/starvation.ts
+++ src/classes/game/processes/character/starvation.ts
@@ -1,9 +1,10 @@
-import { Process, When } from '@/classes/game/base/processes';
+import { Process, When, IgnoreLimits } from '@/classes/game/base/processes';
 import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
 
 @When(process => process.stats.character.energy.value.isZero() || process.stats.character.stomach.value.isZero())
 export class Starvation extends Process {
+  @IgnoreLimits('lessThanMinimum')
   drainHealth = new Mutation(() => this.stats.character.health, () => {
     return new Decimal(-1).div(this.modifiers.character.healthPreservationMultiplier.value);
   });
