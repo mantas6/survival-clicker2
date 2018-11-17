436d13cfab8698cfdca319046253c2f56ad071db
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-17 09:14:00 +0200

    Set max purchase count to 5

--- src/classes/game/actions/education/primary/school.ts
+++ src/classes/game/actions/education/primary/school.ts
@@ -1,10 +1,11 @@
-import { Action, Unlocks } from '@/classes/game/base/actions';
+import { Action, Unlocks, VisibleWhen } from '@/classes/game/base/actions';
 import { Mutation } from '@/classes/game/base/mutations';
 import { SerializeAllOn } from '@/classes/game/base/serialization';
 import { Effect } from '@/classes/game/base/modifiers';
 import Decimal from 'decimal.js';
 
 @SerializeAllOn('emit')
+@VisibleWhen(action => action.stats.education.school.value.lessThan(5))
 export class School extends Action {
   @Unlocks
   money = new Mutation(() => this.stats.finance.money, () => {
