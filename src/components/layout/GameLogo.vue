56d0cde5c9cb84d2c3b25494b7ec97b0c0c28fc3
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-23 10:44:08 +0200

    Experimental fix for loading SVGs

--- src/components/layout/GameLogo.vue
+++ src/components/layout/GameLogo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="logo">
     <span>Survival</span>
-    <img src="@/assets/logo.svg">
+    <span v-html="content"></span>
     <b>2</b>
   </div>
 </template>
@@ -9,22 +9,37 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-@Component
+@Component({
+  mounted(this: GameLogo) {
+    const url = require('@/assets/logo.svg');
+    fetch(url)
+      .then(res => res.text())
+      .then(text => this.content = text);
+  }
+})
 export default class GameLogo extends Vue {
+  content = '';
 
+  get img() {
+    return require('@/assets/logo.svg');
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .logo {
     width: 100%;
     padding: 0 1.5vw;
     font-size: 3.75vw;
 
-    img {
+    svg {
       height: 4.9875vw;
       margin: 0 -1.1vw 0 -0.75vw;
       transform: translateY(12.5%);
+
+      .dark-mode & {
+        fill: white;
+      }
     }
   }
 </style>
