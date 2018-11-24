05ca522200e87acfd3e3c8b9db2a4cf7062028b8
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-11-24 14:10:31 +0200

    Add 4th stage job

---
+++ src/classes/game/actions/jobs/information-technology/create-react-app.ts
@@ -0,0 +1,14 @@
+import { Action, UnlocksWhen } from '@/classes/game/base/actions';
+import { Mutation } from '@/classes/game/base/mutations';
+import Decimal from 'decimal.js';
+
+@UnlocksWhen(action => action.stats.education.informationTechnology.value.greaterThanOrEqualTo(4))
+export class CreateReactApp extends Action {
+  energy = new Mutation(() => this.stats.character.energy, () => {
+    return new Decimal(-30).div(this.modifiers.character.concentration.value);
+  });
+
+  money = new Mutation(() => this.stats.finance.money, () => {
+    return new Decimal(300);
+  });
+}
--- src/classes/game/actions/jobs/information-technology/index.ts
+++ src/classes/game/actions/jobs/information-technology/index.ts
@@ -1,7 +1,11 @@
 import { Serializable, SerializeAllOn } from '@/classes/game/base/serialization';
 import { CraftJankyWebsite } from './craft-janky-website';
+import { CraftResponsiveWebsite } from './craft-responsive-website';
+import { CreateReactApp } from './create-react-app';
 
 @SerializeAllOn('emit', 'store')
 export class InformationTechnology extends Serializable {
   craftJankyWebsite = new CraftJankyWebsite();
+  craftResponsiveWebsite = new CraftResponsiveWebsite();
+  createReactApp = new CreateReactApp();
 }
