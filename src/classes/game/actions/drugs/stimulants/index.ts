724000a88c923ab5e59f29e0c59b12dd4e0c5678
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-12-01 13:49:43 +0200

    Add concentration stimulant

--- src/classes/game/actions/drugs/stimulants/index.ts
+++ src/classes/game/actions/drugs/stimulants/index.ts
@@ -2,10 +2,12 @@ import { Serializable, SerializeAllOn } from '@/classes/game/base/serialization'
 import { CheapStimulant } from './cheap-stimulant';
 import { EnergyDrink } from './energy-drink';
 import { Coffee } from './coffee';
+import { Modafinil } from './modafinil';
 
 @SerializeAllOn('emit', 'store')
 export class Stimulants extends Serializable {
   coffee = new Coffee();
   energyDrink = new EnergyDrink();
   cheapStimulant = new CheapStimulant();
+  modafinil = new Modafinil();
 }
---
+++ src/classes/game/actions/drugs/stimulants/modafinil.ts
@@ -0,0 +1,27 @@
+import { Action, UnlocksWhen } from '@/classes/game/base/actions';
+import { Mutation } from '@/classes/game/base/mutations';
+import { Effect } from '@/classes/game/base/modifiers';
+import Decimal from 'decimal.js';
+
+@UnlocksWhen(action => action.modifiers.education.medicine.value.greaterThanOrEqualTo(2))
+export class Modafinil extends Action {
+  drainEnergy = new Effect({
+    modifier: () => this.modifiers.character.concentration,
+    duration: () => 10,
+    value: () => new Decimal(0.1),
+  });
+
+  stomach = new Mutation(() => this.stats.character.stomach, () => {
+    return new Decimal(1);
+  });
+
+  hydration = new Effect({
+    modifier: () => this.modifiers.character.intake.hydration,
+    duration: () => 60,
+    value: () => new Decimal(-0.1),
+  });
+
+  money = new Mutation(() => this.stats.finance.money, () => {
+    return this.helpers.moneyCost(10);
+  });
+}
