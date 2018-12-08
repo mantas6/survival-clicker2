128bd45591607e6ea6d0d9bd7803feaabac21889
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-12-08 09:46:59 +0200

    Include construction

--- src/classes/game/actions/jobs/index.ts
+++ src/classes/game/actions/jobs/index.ts
@@ -2,10 +2,12 @@ import { Serializable, SerializeAllOn } from '@/classes/game/base/serialization'
 import { Educationless } from './educationless';
 import { Donor } from './donor';
 import { InformationTechnology } from './information-technology';
+import { Construction } from './construction';
 
 @SerializeAllOn('emit', 'store')
 export class Jobs extends Serializable {
   educationless = new Educationless();
   informationTechnology = new InformationTechnology();
+  construction = new Construction();
   donor = new Donor();
 }
