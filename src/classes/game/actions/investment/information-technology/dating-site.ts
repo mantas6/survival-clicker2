c4543b4b94afd04ae3fbf209bdeacf96b3172b22
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-12-08 09:14:39 +0200

    Add dating site investment

---
+++ src/classes/game/actions/investment/information-technology/dating-site.ts
@@ -0,0 +1,16 @@
+import { Action, UnlocksWhen } from '@/classes/game/base/actions';
+import { Mutation } from '@/classes/game/base/mutations';
+import { SerializeAllOn } from '@/classes/game/base/serialization';
+import Decimal from 'decimal.js';
+
+@SerializeAllOn('emit')
+@UnlocksWhen(action => action.modifiers.education.informationTechnology.value.greaterThanOrEqualTo(5))
+export class DatingSite extends Action {
+  money = new Mutation(() => this.stats.finance.money, () => {
+    return this.helpers.growthMoneyCost(this.timesCalculated, 5000, 1.05);
+  });
+
+  investment = new Mutation(() => this.stats.finance.investment, () => {
+    return new Decimal(100);
+  });
+}
--- src/classes/game/actions/investment/information-technology/index.ts
+++ src/classes/game/actions/investment/information-technology/index.ts
@@ -1,9 +1,11 @@
 import { Serializable, SerializeAllOn } from '@/classes/game/base/serialization';
 import { UnfairLottery } from './unfair-lottery';
 import { LinuxServer } from './linux-server';
+import { DatingSite } from './dating-site';
 
 @SerializeAllOn('emit', 'store')
 export class InformationTechnology extends Serializable {
   unfairLottery = new UnfairLottery();
   linuxServer = new LinuxServer();
+  datingSite = new DatingSite();
 }
