a932b9d6db45abfcea904d38ade43d976210404e
Mantas Pauliukonis <mantas@publicsecrets.lt>
2018-12-10 08:01:09 +0200

    Increase potency and decrease dehydration of digestion pills

--- src/classes/game/actions/drugs/digestion/digestion-pills.ts
+++ src/classes/game/actions/drugs/digestion/digestion-pills.ts
@@ -7,14 +7,14 @@ import Decimal from 'decimal.js';
 export class DigestionPills extends Action {
   hydration = new Effect({
     modifier: () => this.modifiers.character.intake.hydration,
-    duration: () => 60,
+    duration: () => 30,
     value: () => new Decimal(-0.1),
   });
 
   digestionSpeed = new Effect({
     modifier: () => this.modifiers.character.digestionSpeed,
     duration: () => 10,
-    value: () => new Decimal(1),
+    value: () => new Decimal(3),
   });
 
   stomach = new Mutation(() => this.stats.character.stomach, () => {
