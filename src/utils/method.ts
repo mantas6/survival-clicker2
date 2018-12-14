24187d6371ea598ab50515aa525509a794de22f8
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-12-14 18:01:36 +0200

    Add split method

--- src/utils/method.ts
+++ src/utils/method.ts
@@ -2,10 +2,12 @@ import get from 'lodash/get';
 import isEmpty from 'lodash/isEmpty';
 import last from 'lodash/last';
 import startCase from 'lodash/startCase';
+import split from 'lodash/split';
 
 export {
   get,
   isEmpty,
   last,
   startCase,
+  split,
 };
