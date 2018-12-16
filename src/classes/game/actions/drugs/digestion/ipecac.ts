e2b269431c7b4aca606a03a068686652809829c6
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-12-16 10:04:22 +0200

    Make ipecac unlock earlier + reduce health loss

--- src/classes/game/actions/drugs/digestion/ipecac.ts
+++ src/classes/game/actions/drugs/digestion/ipecac.ts
@@ -3,11 +3,11 @@ import { Action, UnlocksWhen } from '@/classes/game/base/actions';
 import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
 
-@UnlocksWhen(action => action.modifiers.education.medicine.knowsBasics())
+@UnlocksWhen(action => action.modifiers.education.school.value.greaterThanOrEqualTo(3))
 export class Ipecac extends Action {
   @IgnoreLimits('lessThanMinimum')
   health = new Mutation(() => this.stats.character.health, () => {
-    return new Decimal(-10).div(this.modifiers.character.healthPreservationMultiplier.value);
+    return new Decimal(-5).div(this.modifiers.character.healthPreservationMultiplier.value);
   });
 
   stomach = new Mutation(() => this.stats.character.stomach, () => {
