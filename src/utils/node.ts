ae56ce6cc969779da42e124e8d3f6684994098e3
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-12-16 12:48:34 +0200

    Remove unused method

--- src/utils/node.ts
+++ src/utils/node.ts
@@ -1,12 +1,5 @@
 import { StateNode } from '@/classes/game/base/state-node';
 
-export function apply<TargetNode extends StateNode>(parentNode: StateNode, applyFunc: (node: TargetNode) => void) {
-  for (const { node } of parentNode.nodes()) {
-    applyFunc(node as TargetNode);
-    apply(node, applyFunc);
-  }
-}
-
 export function* traverse(parentNode: StateNode): IterableIterator<StateNode> {
   for (const { node } of parentNode.nodes()) {
     // parentNode own properties
