b6e86ec0dcd187d4668385aed4ba9feead67e44d
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-12-25 16:14:03 +0200

    Increase blood donor payout

--- src/classes/game/actions/jobs/donor/sell-blood.ts
+++ src/classes/game/actions/jobs/donor/sell-blood.ts
@@ -5,7 +5,7 @@ import Decimal from 'decimal.js';
 @UnlocksWhen(action => !action.modifiers.education.bloodTest.value.isZero())
 export class SellBlood extends Action {
   money = new Mutation(() => this.stats.finance.money, () => {
-    return new Decimal(200).times(this.stats.character.health.level).floor();
+    return new Decimal(300).times(this.stats.character.health.level).floor();
   });
 
   health = new Mutation(() => this.stats.character.health, () => {
