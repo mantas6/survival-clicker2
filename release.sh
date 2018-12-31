8747eaa3fd2e405854d1be9eab44cbbf15142d26
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-12-31 14:15:23 +0200

    Add release self-deployment script

---
+++ release.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+archive_name='dist.tar.gz'
+
+while true
+do
+  echo "Fetching data"
+
+  data=$(curl -s 'https://api.github.com/repos/MantasPauliukonis/survival-clicker2/releases/latest')
+
+  url=$(echo $data | jq -r '.assets[0].browser_download_url')
+  version=$(echo $data | jq -r '.tag_name')
+
+  echo "Remote version is $version"
+
+  if [ "$version" != "$previous_version" ]; then
+    echo "Version mismatch $version != $previous_version"
+
+    rm $archive_name
+
+    echo "Downloading"
+
+    if ! wget -O $archive_name $url; then
+      exit 1
+    fi
+
+    rm -rf dist
+
+    echo "Extracting"
+
+    if ! tar xvzf $archive_name; then
+      exit 1
+    fi
+
+    echo "Deploying"
+
+    rm -rf public_html/*
+    cp -a dist/. public_html
+
+    previous_version=$version
+  else
+    echo "Already up-to-date $version"
+  fi
+
+  sleep 30
+done
