387bb39cd129accf770f6be4d658b37ef94bf57f
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-01-05 14:26:59 +0200

    Update to 0.1.4

--- src/components/layout/HeaderContainer.vue
+++ src/components/layout/HeaderContainer.vue
@@ -21,7 +21,7 @@
         <container-stat name="stomach" :stat="stomach"></container-stat>
       </div>
     </div>
-    <small class="version">0.1.3</small>
+    <small class="version">0.1.4</small>
     <div class="controls">
       <button @click="pause">
         <template v-if="globals.isPaused">{{ $t('unpause') }}</template>
