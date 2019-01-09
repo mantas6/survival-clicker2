adbe95b7713e67a4659f4890d5d999117918741b
Mantas Pauliukonis <mantas@publicsecrets.lt>
2019-01-09 13:44:04 +0200

    Add comments

--- src/classes/game/base/transformable.ts
+++ src/classes/game/base/transformable.ts
@@ -6,6 +6,11 @@ export interface TransformationDescriptor {
   valueFunc: () => any;
 }
 
+/**
+ * Sets property value when transformation is called
+ * @param tagName transformation tag name
+ * @param valueFunc value that will be set on transformation
+ */
 export function Transform<Target>(tagName: string, valueFunc: () => Target) {
   return (transformableClass: Transformable, propertyName: string) => {
     const ctor = transformableClass.constructor;
@@ -16,6 +21,11 @@ export function Transform<Target>(tagName: string, valueFunc: () => Target) {
   };
 }
 
+/**
+ * Excludes properties from transformation
+ * May be used to exclude transformations from parent class
+ * @param tagNames tag names to skip
+ */
 export function SkipTransform(...tagNames: string[]) {
   return (transformableClass: Transformable, propertyName: string) => {
     const ctor = transformableClass.constructor;
