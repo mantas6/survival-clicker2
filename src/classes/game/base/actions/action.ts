e4e1b5984e88c7b61c285cf4b8ed40079b056f0a
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-01-19 11:53:11 +0200

    Remove incorrect import

--- src/classes/game/base/actions/action.ts
+++ src/classes/game/base/actions/action.ts
@@ -3,7 +3,7 @@ import { SerializeOn, UnserializeAs } from '@/classes/game/base/serialization';
 import Decimal from 'decimal.js';
 import { TagName } from '@/classes/game/base/serialization/serializable';
 import { Calculable, Mutation, ValidationOptions, CalculationOptions } from '@/classes/game/base/mutations';
-import { Transform, Touch } from '@/classes/game/base/transformable';
+import { Transform } from '@/classes/game/base/transformable';
 import { Queued } from '@/classes/game/base/automation';
 
 export type ConditionFunction = (action: Action, opts: ValidationOptions) => boolean;
