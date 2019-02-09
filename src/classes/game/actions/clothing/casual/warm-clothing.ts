caa25acc5ff7591def4caed19248f6c508705bbe
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-02-09 12:28:38 +0200

    Add insulation effect

--- src/classes/game/actions/clothing/casual/warm-clothing.ts
+++ src/classes/game/actions/clothing/casual/warm-clothing.ts
@@ -1,6 +1,7 @@
 import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
 import { ClothingAction } from '@/classes/game/base/actions/clothing-action';
+import { Effect } from '@/classes/game/base/modifiers';
 
 export class WarmClothing extends ClothingAction {
   money = new Mutation(() => this.stats.finance.money, () => {
@@ -9,5 +10,10 @@ export class WarmClothing extends ClothingAction {
     }
 
     return this.helpers.moneyCost(10);
+  });
+
+  insulation = new Effect({
+    modifier: () => this.modifiers.character.thermoregulation.insulation,
+    value: () => new Decimal(10),
   });
 }
