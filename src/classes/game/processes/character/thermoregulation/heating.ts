1072ed96b54a5d9a5d23469e7f724a4277d120e2
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-02-09 15:50:11 +0200

    Adjust temp range

--- src/classes/game/processes/character/thermoregulation/heating.ts
+++ src/classes/game/processes/character/thermoregulation/heating.ts
@@ -3,7 +3,7 @@ import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
 import { TemperatureMutation } from '@/classes/game/base/templates/mutations/temperature-mutation';
 
-@When(process => process.stats.character.temperature.value.lessThan(36.4))
+@When(process => process.stats.character.temperature.value.lessThan(36.6))
 export class Heating extends Process {
   temperature = new TemperatureMutation(() => this.stats.character.temperature, () => {
     return new Decimal(0.1);
