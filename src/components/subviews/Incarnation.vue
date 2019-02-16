3fe702fcc0d8a8990ff92badf0e03d97f1cdf259
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-02-16 12:40:28 +0200

    Reuse action view on incarnation view

--- src/components/subviews/Incarnation.vue
+++ src/components/subviews/Incarnation.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <navigation @selectCategory="activeCategory = $event"
+      :unseen-categories="unseenCategoryNames"
+      :available-categories="availableCategoryNames">
+    </navigation>
+    <article>
+      <action-group v-for="(group, groupName) of availableGroups"
+        :key="groupName"
+        :groupName="groupName"
+        :categoryName="activeCategory"
+        :group="group">
+      </action-group>
+    </article>
     <div v-if="stats">
       <span>Incarnation points: </span>
       <number-format :value="incarnationPoints"></number-format>
@@ -11,16 +23,55 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { Getter } from 'vuex-class';
+import ActionGroup from '@/components/layout/Actions/ActionGroup.vue';
+import Navigation from '@/components/layout/Navigation.vue';
 import { Relay } from '@/classes/relay';
 import { SerializedStats } from '@/store/stats';
+import { SerializedActions } from '@/store/actions';
+import { pickBy } from '@/utils/method';
 
-@Component
+@Component({
+  components: { ActionGroup, Navigation },
+})
 export default class Incarnation extends Vue {
   @Getter relay!: Relay;
   @Getter stats!: SerializedStats;
+  @Getter allActions!: SerializedActions;
+
+  activeCategory: 'modules' = 'modules';
 
   get incarnationPoints() {
     return this.stats.incarnation.points.value;
+  }
+
+  get availableCategoryNames() {
+    if (this.allActions && this.allActions.incarnation) {
+      return Object.keys(this.allActions.incarnation);
+    }
+  }
+
+  get unseenCategoryNames(): string[] {
+    const categoriesNames = [];
+
+    if (this.allActions && this.allActions.incarnation) {
+      for (const [ categoryName, { hasUnseen } ] of Object.entries(this.allActions.incarnation)) {
+        if (hasUnseen) {
+          categoriesNames.push(categoryName);
+        }
+      }
+    }
+
+    return categoriesNames;
+  }
+
+  get availableGroups() {
+    if (!this.allActions || !this.allActions.incarnation) {
+      return;
+    }
+
+    const groups = this.allActions.incarnation[this.activeCategory];
+
+    return pickBy<{}>(groups, group => Object.keys(group).length);
   }
 
   reset() {
@@ -30,6 +81,12 @@ export default class Incarnation extends Vue {
 </script>
 
 <style lang="scss" scoped>
+  article {
+    display: grid;
+    grid-gap: 1rem;
+    width: 50%;
+  }
+
   button {
     background: transparent;
     color: black;
