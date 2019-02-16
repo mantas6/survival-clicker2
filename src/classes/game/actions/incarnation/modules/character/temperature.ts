6b37302b146543e82d7eb94105dd5e485e0b03c0
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-02-16 14:05:09 +0200

    Disable multiplier

--- src/classes/game/actions/incarnation/modules/character/temperature.ts
+++ src/classes/game/actions/incarnation/modules/character/temperature.ts
@@ -1,7 +1,8 @@
 import Decimal from 'decimal.js';
 import { Mutation } from '@/classes/game/base/mutations/mutation';
-import { ToggleAction } from '@/classes/game/base/actions';
+import { ToggleAction, NoMultiplier } from '@/classes/game/base/actions';
 
+@NoMultiplier
 export class Temperature extends ToggleAction {
   points = new Mutation(() => this.stats.incarnation.points, () => {
     if (this.timesCalculated.greaterThan(0)) {
