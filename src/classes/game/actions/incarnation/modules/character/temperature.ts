a7e2356f64f58c04ae8ae4b3725f875cc46a3e45
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-02-16 12:42:56 +0200

    Fix parent class, set buy once

--- src/classes/game/actions/incarnation/modules/character/temperature.ts
+++ src/classes/game/actions/incarnation/modules/character/temperature.ts
@@ -1,9 +1,13 @@
 import Decimal from 'decimal.js';
-import { ClothingAction } from '@/classes/game/base/actions/clothing-action';
 import { Mutation } from '@/classes/game/base/mutations/mutation';
+import { ToggleAction } from '@/classes/game/base/actions';
 
-export class Temperature extends ClothingAction {
+export class Temperature extends ToggleAction {
   points = new Mutation(() => this.stats.incarnation.points, () => {
+    if (this.timesCalculated.greaterThan(0)) {
+      return new Decimal(0);
+    }
+
     return new Decimal(-50);
   });
 }
