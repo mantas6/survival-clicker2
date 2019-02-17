37b04de1f61a63c943c1af9522e68c2c47fe4e6b
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-02-17 10:21:12 +0200

    Implement persistence

--- src/classes/game/actions/incarnation/modules/character/temperature.ts
+++ src/classes/game/actions/incarnation/modules/character/temperature.ts
@@ -1,8 +1,9 @@
 import Decimal from 'decimal.js';
 import { Mutation } from '@/classes/game/base/mutations/mutation';
-import { ToggleAction, NoMultiplier } from '@/classes/game/base/actions';
+import { ToggleAction, NoMultiplier, Persist } from '@/classes/game/base/actions';
 
 @NoMultiplier
+@Persist
 export class Temperature extends ToggleAction {
   points = new Mutation(() => this.stats.incarnation.points, () => {
     if (this.timesCalculated.greaterThan(0)) {
--- src/classes/game/stats/incarnation/points.ts
+++ src/classes/game/stats/incarnation/points.ts
@@ -1,5 +1,6 @@
-import { Value } from '@/classes/game/base/stats';
+import { Value, Persist } from '@/classes/game/base/stats';
 
+@Persist
 export class Points extends Value {
   readonly default = 0;
 }
