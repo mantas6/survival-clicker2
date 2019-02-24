2e792e8d3dc9c325226550fede41153627b99313
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-02-24 10:59:25 +0200

    Add head method

--- src/utils/method.ts
+++ src/utils/method.ts
@@ -1,5 +1,6 @@
 import get from 'lodash/get';
 import isEmpty from 'lodash/isEmpty';
+import head from 'lodash/head';
 import last from 'lodash/last';
 import startCase from 'lodash/startCase';
 import split from 'lodash/split';
@@ -8,6 +9,7 @@ import pickBy from 'lodash/pickBy';
 export {
   get,
   isEmpty,
+  head,
   last,
   startCase,
   split,
