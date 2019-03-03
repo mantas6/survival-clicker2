5173c789c5c16ff399f91f66f2fc88545d82d29f
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-03 09:49:43 +0200

    Add take method

--- src/utils/method.ts
+++ src/utils/method.ts
@@ -6,7 +6,16 @@ import startCase from 'lodash/startCase';
 import split from 'lodash/split';
 import pickBy from 'lodash/pickBy';
 
+function take<Result>(callback: () => Result): Result | undefined {
+  try {
+    return callback();
+  } catch {
+    return undefined;
+  }
+}
+
 export {
+  take,
   get,
   isEmpty,
   head,
