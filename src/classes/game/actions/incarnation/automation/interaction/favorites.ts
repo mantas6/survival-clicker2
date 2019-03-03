164df913e4bc8bfe584b39231c31babfdaa9631c
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-03 12:35:04 +0200

    Add point unlock requirements

--- src/classes/game/actions/incarnation/automation/interaction/favorites.ts
+++ src/classes/game/actions/incarnation/automation/interaction/favorites.ts
@@ -1,11 +1,12 @@
 import Decimal from 'decimal.js';
 import { Mutation } from '@/classes/game/base/mutations/mutation';
-import { ToggleAction, NoMultiplier, Persist, NoFavorites } from '@/classes/game/base/actions';
+import { ToggleAction, NoMultiplier, Persist, NoFavorites, Unlocks } from '@/classes/game/base/actions';
 
 @NoMultiplier
 @Persist
 @NoFavorites
 export class Favorites extends ToggleAction {
+  @Unlocks
   points = new Mutation(() => this.stats.incarnation.points, () => {
     if (this.timesCalculated.greaterThan(0)) {
       return new Decimal(0);
--- src/classes/game/actions/incarnation/automation/interaction/queue.ts
+++ src/classes/game/actions/incarnation/automation/interaction/queue.ts
@@ -1,12 +1,13 @@
 import Decimal from 'decimal.js';
 import { Mutation } from '@/classes/game/base/mutations/mutation';
-import { ToggleAction, NoMultiplier, Persist, NoFavorites, UnlocksWhen } from '@/classes/game/base/actions';
+import { ToggleAction, NoMultiplier, Persist, NoFavorites, UnlocksWhen, Unlocks } from '@/classes/game/base/actions';
 
 @NoMultiplier
 @Persist
 @NoFavorites
 @UnlocksWhen(action => action.actions.incarnation.automation.interaction.favorites.isCalculatedOnce)
 export class Queue extends ToggleAction {
+  @Unlocks
   points = new Mutation(() => this.stats.incarnation.points, () => {
     if (this.timesCalculated.greaterThan(0)) {
       return new Decimal(0);
