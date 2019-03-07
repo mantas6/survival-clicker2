a588bd599b83c561bb6151438a32961f9b3ec109
Mantas Pauliukonis <mantas@publicsecrets.lt>
2019-03-07 12:30:48 +0200

    Remove unused import

--- src/classes/game/actions/other/character/reincarnate.ts
+++ src/classes/game/actions/other/character/reincarnate.ts
@@ -1,7 +1,7 @@
 import { Action } from '@/classes/game/base/actions';
 import { CalculationOptions } from '@/classes/game/base/mutations';
 import { SerializeAllOn } from '@/classes/game/base/serialization';
-import { Transformable, applyReset } from '@/classes/game/base/transformable';
+import { applyReset } from '@/classes/game/base/transformable';
 
 @SerializeAllOn('emit')
 export class Reincarnate extends Action {
