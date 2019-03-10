5b2e51452dc87f6abf972074c524f7c7dd9ee7f8
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-10 11:06:08 +0200

    Implement a test auto when toggled action type

--- src/classes/game/actions/mundane/jobs/educationless/index.ts
+++ src/classes/game/actions/mundane/jobs/educationless/index.ts
@@ -1,9 +1,11 @@
 import { Serializable, SerializeAllOn } from '@/classes/game/base/serialization';
 import { Janitor } from './janitor';
+import { JanitorDaily } from './janitor-daily';
 import { PizzaDelivery } from './pizza-delivery';
 
 @SerializeAllOn('emit', 'store')
 export class Educationless extends Serializable {
   janitor = new Janitor();
+  janitorDaily = new JanitorDaily();
   pizzaDelivery = new PizzaDelivery();
 }
---
+++ src/classes/game/actions/mundane/jobs/educationless/janitor-daily.ts
@@ -0,0 +1,21 @@
+import { IgnoreLimits } from '@/classes/game/base/processes';
+import { ToggleAction, AutoWhenToggled } from '@/classes/game/base/actions';
+import { Mutation } from '@/classes/game/base/mutations';
+import Decimal from 'decimal.js';
+import { TemperatureMutation } from '@/classes/game/base/templates/mutations/temperature-mutation';
+
+@AutoWhenToggled
+export class JanitorDaily extends ToggleAction {
+  @IgnoreLimits('lessThanMinimum')
+  stamina = new Mutation(() => this.stats.character.stamina, () => {
+    return new Decimal(-1);
+  });
+
+  temperature = new TemperatureMutation(() => this.stats.character.temperature, () => {
+    return new Decimal(0.02);
+  });
+
+  money = new Mutation(() => this.stats.finance.money, () => {
+    return new Decimal(1);
+  });
+}
