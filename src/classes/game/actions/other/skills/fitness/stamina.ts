368e73c907befa4b8057c4fd81587add35f58dbe
Mantas Pauliukonis <mantas@publicsecrets.lt>
2019-03-14 08:12:32 +0200

    Remove unused import

--- src/classes/game/actions/other/skills/fitness/stamina.ts
+++ src/classes/game/actions/other/skills/fitness/stamina.ts
@@ -1,5 +1,4 @@
 import { Action, UnlocksWhen, NoMultiplier } from '@/classes/game/base/actions';
-import { When } from '@/classes/game/base/processes';
 
 @NoMultiplier
 @UnlocksWhen(action => action.stats.skills.fitness.value.greaterThan(0))
