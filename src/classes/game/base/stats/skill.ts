34b077506bd81b2bc9f77ddca7e40fc259f756b2
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-15 11:13:27 +0200

    Add skill prototype class

---
+++ src/classes/game/base/stats/skill.ts
@@ -0,0 +1,35 @@
+/* tslint:disable:new-parens */
+
+import { Transformable } from '@/classes/game/base/transformable';
+import { Value } from '.';
+import Decimal from 'decimal.js';
+import { SerializeOn } from '@/classes/game/base/serialization';
+
+export abstract class Skill extends Transformable {
+  @SerializeOn('emit', 'store')
+  level = new (class Level extends Value {
+    readonly default = 1;
+  });
+
+  @SerializeOn('emit', 'store')
+  experience = new (class Experience extends Value {
+    readonly default = 0;
+  });
+
+  onClock() {
+    super.onClock();
+    this.checkLevelUp();
+  }
+
+  protected checkLevelUp() {
+    if (this.experience.value.greaterThanOrEqualTo(this.experienceRequired)) {
+      this.level.mutate(value => value.add(1));
+      this.experience.mutate(value => value.sub(value));
+    }
+  }
+
+  @SerializeOn('emit')
+  get experienceRequired(): Decimal {
+    return this.level.value.add(100).pow(3);
+  }
+}
