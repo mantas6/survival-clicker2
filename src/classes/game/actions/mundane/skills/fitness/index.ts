b90967f79f97dddd05468d16b384d6bcefb6b2f7
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-16 09:13:10 +0200

    Add skill indentifier

--- src/classes/game/actions/mundane/skills/fitness/index.ts
+++ src/classes/game/actions/mundane/skills/fitness/index.ts
@@ -1,7 +1,12 @@
-import { Serializable, SerializeAllOn } from '@/classes/game/base/serialization';
+import { SerializeAllOn, SerializableWithReference, SerializeOn } from '@/classes/game/base/serialization';
 import { Workout } from './workout';
 
 @SerializeAllOn('emit', 'store')
-export class Fitness extends Serializable {
+export class Fitness extends SerializableWithReference {
   workout = new Workout();
+
+  @SerializeOn('emit')
+  get skill() {
+    return this.stats.skills.fitness;
+  }
 }
