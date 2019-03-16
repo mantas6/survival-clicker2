080c03142237cf045ac8e7e6bb7209bd7fd81d60
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-16 10:00:29 +0200

    Add customized skill group view

---
+++ src/components/layout/Actions/Skills/SkillActionGroup.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <small>{{ $t(`actions.${categoryName}.groups.${groupName}.title`) }}</small>
+    <action-item v-for="(action, actionName) of group"
+      :key="actionName"
+      :item="action">
+    </action-item>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import ActionItem from '@/components/layout/Actions/ActionItem.vue';
+
+@Component({ components: { ActionItem } })
+export default class ActionGroup extends Vue {
+  @Prop({ required: true })
+  group!: { [actionName: string]: {} };
+
+  @Prop({ required: true })
+  categoryName!: string;
+
+  @Prop({ required: true })
+  groupName!: string;
+}
+</script>
+
+<style lang="scss" scoped>
+  small {
+    color: grey;
+  }
+</style>
