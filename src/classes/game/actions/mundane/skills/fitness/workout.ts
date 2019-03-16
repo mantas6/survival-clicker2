0a6ee3165c408398b4b536978037dc309ed182a0
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-16 12:24:18 +0200

    Add unlock threshold

--- src/classes/game/actions/mundane/skills/fitness/workout.ts
+++ src/classes/game/actions/mundane/skills/fitness/workout.ts
@@ -1,7 +1,8 @@
-import { Action } from '@/classes/game/base/actions';
+import { Action, UnlocksWhen } from '@/classes/game/base/actions';
 import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
 
+@UnlocksWhen(action => action.stats.skills.fitness.level.value.greaterThanOrEqualTo(2))
 export class Workout extends Action {
   money = new Mutation(() => this.stats.finance.money, () => {
     return new Decimal(300).neg();
