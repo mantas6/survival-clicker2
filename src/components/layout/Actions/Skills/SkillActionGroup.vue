b013e909602c63900a0fcfd9721f2dce620c1c12
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-16 12:24:26 +0200

    Add no perks label

--- src/components/layout/Actions/Skills/SkillActionGroup.vue
+++ src/components/layout/Actions/Skills/SkillActionGroup.vue
@@ -5,13 +5,18 @@
       <small>Lvl: {{ group.skill.level.value }}</small>
       <small>XP: {{ group.skill.experience.value }} / {{ group.skill.experienceRequired }}</small>
     </div>
-    <template v-for="(action, actionName) of group">
-      <action-item
-        v-if="actionName != 'skill'"
-        :key="actionName"
-        :item="action">
-      </action-item>
+    <template v-if="hasPerks">
+      <template v-for="(action, actionName) of group">
+        <action-item
+          v-if="actionName != 'skill'"
+          :key="actionName"
+          :item="action">
+        </action-item>
+      </template>
     </template>
+    <div v-else class="empty">
+      <i>No perks unlocked</i>
+    </div>
   </section>
 </template>
 
@@ -29,6 +34,10 @@ export default class ActionGroup extends Vue {
 
   @Prop({ required: true })
   groupName!: string;
+
+  get hasPerks() {
+    return Object.keys(this.group).length > 1;
+  }
 }
 </script>
 
@@ -40,5 +49,9 @@ export default class ActionGroup extends Vue {
       margin-right: 0.5rem;
       color: grey;
     }
+  }
+
+  .empty {
+    margin-top: 0.75rem;
   }
 </style>
