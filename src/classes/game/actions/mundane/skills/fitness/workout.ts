f7db0efc863f607fa667bcc975e0da9f9e3b8756
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-17 11:07:05 +0200

    Reduce cost, allow only single purchase

--- src/classes/game/actions/mundane/skills/fitness/workout.ts
+++ src/classes/game/actions/mundane/skills/fitness/workout.ts
@@ -1,10 +1,12 @@
 import { Action, UnlocksWhen } from '@/classes/game/base/actions';
 import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
+import { When } from '@/classes/game/base/processes';
 
 @UnlocksWhen(action => action.stats.skills.fitness.level.value.greaterThanOrEqualTo(2))
+@When<Action>(action => !action.isCalculatedOnce)
 export class Workout extends Action {
   money = new Mutation(() => this.stats.finance.money, () => {
-    return new Decimal(300).neg();
+    return new Decimal(50).neg();
   });
 }
