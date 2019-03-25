e28e04c8aae06d3c7d53b3c8555c2d9fea548737
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-25 10:06:50 +0200

    Add total level counter

--- src/classes/game/stats/skills/index.ts
+++ src/classes/game/stats/skills/index.ts
@@ -1,7 +1,23 @@
-import { Serializable, SerializeAllOn } from '@/classes/game/base/serialization';
+import { Serializable, SerializeAllOn, SerializeOn } from '@/classes/game/base/serialization';
 import { Fitness } from './fitness';
+import { traverse } from '@/utils/node';
+import { Skill } from '@/classes/game/base/stats';
+import Decimal from 'decimal.js';
 
 @SerializeAllOn('emit', 'store')
 export class Skills extends Serializable {
   fitness = new Fitness();
+
+  @SerializeOn('emit')
+  get totalLevel() {
+    let total = new Decimal(0);
+
+    for (const node of traverse(this)) {
+      if (node instanceof Skill) {
+        total = total.add(node.level.value);
+      }
+    }
+
+    return total;
+  }
 }
