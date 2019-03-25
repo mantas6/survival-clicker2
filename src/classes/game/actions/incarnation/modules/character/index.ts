41dea7640a37e28eb674a5334f34edc9e7f3f6c3
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-25 11:12:37 +0200

    Add skills module unlockable

--- src/classes/game/actions/incarnation/modules/character/index.ts
+++ src/classes/game/actions/incarnation/modules/character/index.ts
@@ -1,7 +1,9 @@
 import { Temperature } from './temperature';
+import { Skills } from './skills';
 import { Serializable, SerializeAllOn } from '@/classes/game/base/serialization';
 
 @SerializeAllOn('emit', 'store')
 export class Character extends Serializable {
   temperature = new Temperature();
+  skills = new Skills();
 }
---
+++ src/classes/game/actions/incarnation/modules/character/skills.ts
@@ -0,0 +1,17 @@
+import Decimal from 'decimal.js';
+import { Mutation } from '@/classes/game/base/mutations/mutation';
+import { ToggleAction, NoMultiplier, Persist, NoFavorites, Unlocks } from '@/classes/game/base/actions';
+
+@NoMultiplier
+@Persist
+@NoFavorites
+export class Skills extends ToggleAction {
+  @Unlocks
+  points = new Mutation(() => this.stats.incarnation.points, () => {
+    if (this.timesCalculated.greaterThan(0)) {
+      return new Decimal(0);
+    }
+
+    return new Decimal(-200);
+  });
+}
