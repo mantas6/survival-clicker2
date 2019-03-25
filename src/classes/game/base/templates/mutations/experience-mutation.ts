8d9b7df3446fc41e58177ed0e18e3afa7f09613b
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-25 11:14:18 +0200

    Set to disable xp when no toggled on

--- src/classes/game/base/templates/mutations/experience-mutation.ts
+++ src/classes/game/base/templates/mutations/experience-mutation.ts
@@ -1,11 +1,16 @@
 import { Mutation } from '@/classes/game/base/mutations';
 import { DiffFunction } from '@/classes/game/base/mutations/mutation';
 import { Value, Skill } from '@/classes/game/base/stats';
+import Decimal from 'decimal.js';
 
 export class ExperienceMutation extends Mutation<Value> {
   constructor(statFunc: () => Skill, diffFunc: DiffFunction) {
     const experienceStatFunc = () => statFunc().experience;
     super(experienceStatFunc, calcOpts => {
+      if (!this.actions.incarnation.modules.character.skills.isToggledOn) {
+        return new Decimal(0);
+      }
+
       const diff = diffFunc(calcOpts);
 
       const finalDiff = diff.div(this.stats.skills.totalLevel.add(1));
