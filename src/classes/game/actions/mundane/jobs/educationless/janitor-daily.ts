c8e0171380c74d0fc8530788c919b226115f540e
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-03-31 14:14:27 +0300

    Set to unlock when school starts

--- src/classes/game/actions/mundane/jobs/educationless/janitor-daily.ts
+++ src/classes/game/actions/mundane/jobs/educationless/janitor-daily.ts
@@ -1,9 +1,10 @@
-import { ToggleAction, AutoWhenToggled } from '@/classes/game/base/actions';
+import { ToggleAction, AutoWhenToggled, UnlocksWhen } from '@/classes/game/base/actions';
 import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
 import { TemperatureMutation } from '@/classes/game/base/templates/mutations/temperature-mutation';
 
 @AutoWhenToggled
+@UnlocksWhen(action => action.modifiers.education.school.hasStarted)
 export class JanitorDaily extends ToggleAction {
   stamina = new Mutation(() => this.stats.character.stamina, () => {
     return new Decimal(-1);
