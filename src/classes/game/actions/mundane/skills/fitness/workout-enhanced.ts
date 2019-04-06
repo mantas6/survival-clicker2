04fe988d55738988a7e0fa0d730c309e40657bd6
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-04-06 15:02:57 +0300

    Disable multiplier and favoriting

--- src/classes/game/actions/mundane/skills/fitness/workout-enhanced.ts
+++ src/classes/game/actions/mundane/skills/fitness/workout-enhanced.ts
@@ -1,10 +1,12 @@
-import { Action, UnlocksWhen } from '@/classes/game/base/actions';
+import { Action, UnlocksWhen, NoMultiplier, NoFavorites } from '@/classes/game/base/actions';
 import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
 import { When } from '@/classes/game/base/processes';
 
 @UnlocksWhen(action => action.stats.skills.fitness.level.value.greaterThanOrEqualTo(5))
 @When<Action>(action => !action.isCalculatedOnce)
+@NoMultiplier
+@NoFavorites
 export class WorkoutEnhanced extends Action {
   learningPoints = new Mutation(() => this.stats.skills.learningPoints, () => {
     return new Decimal(-3);
--- src/classes/game/actions/mundane/skills/fitness/workout.ts
+++ src/classes/game/actions/mundane/skills/fitness/workout.ts
@@ -1,10 +1,12 @@
-import { Action, UnlocksWhen } from '@/classes/game/base/actions';
+import { Action, UnlocksWhen, NoMultiplier, NoFavorites } from '@/classes/game/base/actions';
 import { Mutation } from '@/classes/game/base/mutations';
 import Decimal from 'decimal.js';
 import { When } from '@/classes/game/base/processes';
 
 @UnlocksWhen(action => action.stats.skills.fitness.level.value.greaterThanOrEqualTo(2))
 @When<Action>(action => !action.isCalculatedOnce)
+@NoMultiplier
+@NoFavorites
 export class Workout extends Action {
   learningPoints = new Mutation(() => this.stats.skills.learningPoints, () => {
     return new Decimal(-1);
