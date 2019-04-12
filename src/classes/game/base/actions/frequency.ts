50d6f8471764713bcca3156616516b98b1ba7842
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-04-12 10:28:33 +0300

    Add configuration

--- src/classes/game/base/actions/frequency.ts
+++ src/classes/game/base/actions/frequency.ts
@@ -1,18 +1,33 @@
 import Decimal from 'decimal.js';
 import { Transformable } from '@/classes/game/base/transformable';
+import { CalculationOptions } from '@/classes/game/base/mutations';
+
+interface FrequencyOptions {
+  riseSensitivity: Decimal.Value;
+  fallSensitivity: Decimal.Value;
+}
 
 export class Frequency extends Transformable {
   private heat = new Decimal(0);
 
-  addUse() {
-    this.heat = this.heat.add(1);
+  private riseSensitivity: Decimal;
+  private fallSensitivity: Decimal;
+
+  constructor(opts: FrequencyOptions) {
+    super();
+    this.riseSensitivity = new Decimal(opts.riseSensitivity);
+    this.fallSensitivity = new Decimal(opts.fallSensitivity);
+  }
+
+  addUse(opts: CalculationOptions) {
+    this.heat = this.heat.add(opts.multiplier.mod(this.riseSensitivity));
   }
 
   onClock() {
     super.onClock();
 
     if (this.heat.greaterThan(0)) {
-      this.heat = this.heat.minus(1);
+      this.heat = this.heat.minus(new Decimal(1).mod(this.fallSensitivity));
     }
   }
 }
