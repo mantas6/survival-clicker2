1396109adc06ddb6eebf9d93ade4e48cf89a4a54
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-04-13 09:23:11 +0300

    Remove standard tolerance

--- src/classes/game/actions/mundane/drugs/stimulants/energy-drink.ts
+++ src/classes/game/actions/mundane/drugs/stimulants/energy-drink.ts
@@ -3,12 +3,9 @@ import { Mutation } from '@/classes/game/base/mutations';
 import { TimerEffect } from '@/classes/game/base/modifiers';
 import Decimal from 'decimal.js';
 import { MoneyCostMutation } from '@/classes/game/base/templates/mutations/money-cost-mutation';
-import { StandardTolerance } from '@/classes/game/base/templates/frequencies/standard-tolerance';
 
 @UnlocksWhen(action => action.modifiers.education.school.hasStarted)
 export class EnergyDrink extends Action {
-  frequency = new StandardTolerance();
-
   hydration = new TimerEffect({
     modifier: () => this.modifiers.character.intake.hydration,
     duration: () => 30,
@@ -18,7 +15,7 @@ export class EnergyDrink extends Action {
   stamina = new TimerEffect({
     modifier: () => this.modifiers.character.regeneration.stamina.restoreSpeedBoost,
     duration: () => 5,
-    value: () => new Decimal(3).div(this.frequency.heat.div(100).add(1)),
+    value: () => new Decimal(3),
   });
 
   stomach = new Mutation(() => this.stats.character.stomach, () => {
