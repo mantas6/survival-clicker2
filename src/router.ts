54c5ffe2dcf3d34c874bcb084c183bacd492b571
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-04-19 10:19:02 +0300

    Add type

--- src/router.ts
+++ src/router.ts
@@ -1,26 +1,29 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import Router, { RouteConfig } from 'vue-router';
 import Actions from './views/Actions.vue';
 import Development from './views/Development.vue';
 
 Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      redirect: { name: 'actions' },
-    },
-    {
-      path: '/actions',
-      name: 'actions',
-      component: Actions,
-    },
-    {
-      path: '/development',
-      name: 'development',
-      component: Development,
-    },
-  ],
-});
+const routes: RouteConfig[] = [
+  {
+    path: '/',
+    name: 'home',
+    redirect: { name: 'actions' },
+  },
+  {
+    path: '/actions',
+    name: 'actions',
+    component: Actions,
+  },
+];
+
+if (process.env.NODE_ENV !== 'production') {
+  routes.push({
+    path: '/development',
+    name: 'development',
+    component: Development,
+  });
+}
+
+export default new Router({ routes });
