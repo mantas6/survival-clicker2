e41414fa75a3c5012ce2f88eb8f0f6715505eaf7
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2019-04-19 09:31:33 +0300

    Remove debug message formatter

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -2,27 +2,10 @@
 
 let isEnabled = false;
 
-export function log(...data: any[]) {
+export function log(...message: any[]) {
   if (isEnabled) {
-    const message = format_message(data);
-
     console.log(...message);
   }
-}
-
-function format_message(data: any[]) {
-  const error = new Error();
-
-  let name = '';
-
-  if (error.stack) {
-    const stack = error.stack.split('\n')
-      .map(line => line.trim());
-
-    name = stack[3];
-  }
-
-  return [ name, ...data ];
 }
 
 export function enableLogging() {
